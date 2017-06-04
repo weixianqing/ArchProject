d0863af90c1257688f7b0688b8ab2bf64b55fdd0
Shelvin Chow <weixianqing@gmail.com>
2017-06-04 12:52:45 +0800

    add two pics

--- src/com/chow/arch/netty/marshallingserial/Client.java
+++ src/com/chow/arch/netty/marshallingserial/Client.java
@@ -1,5 +1,6 @@
 package com.chow.arch.netty.marshallingserial;
 
+import com.chow.arch.utils.GzipUtils;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -8,12 +9,17 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * Created by shelvin chow on 2017/6/1.
  */
 public class Client
 {
-    public static void main(String[] args) throws InterruptedException
+    public static void main(String[] args) throws InterruptedException, IOException
     {
         EventLoopGroup group = new NioEventLoopGroup();
         Bootstrap client = new Bootstrap();
@@ -31,14 +37,24 @@ public class Client
                     }
                 });
 
-        ChannelFuture channelFuture = client.connect("127.0.0.1",8765).sync();
+        ChannelFuture channelFuture = client.connect("127.0.0.1", 8765).sync();
 
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 2; i++)
         {
             Req req = new Req();
             req.setId("" + i);
             req.setName("pro" + i);
             req.setRequestMessage("data msg " + i);
+
+
+            String readPath = System.getProperty("user.dir") + File.separatorChar
+                    + "sources" + File.separatorChar+ "00" + i + ".jpg";
+            File file = new File(readPath);
+            FileInputStream fis = new FileInputStream(file);
+            byte[] data = new byte[fis.available()];
+            fis.read(data);
+            fis.close();
+            req.setAttachment(GzipUtils.gzip(data));
             channelFuture.channel().writeAndFlush(req);
         }
         channelFuture.channel().closeFuture().sync();
--- src/com/chow/arch/netty/marshallingserial/ServerHandler.java
+++ src/com/chow/arch/netty/marshallingserial/ServerHandler.java
@@ -1,7 +1,12 @@
 package com.chow.arch.netty.marshallingserial;
 
+import com.chow.arch.utils.GzipUtils;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by shelvin chow on 2017/6/1.
@@ -14,11 +19,24 @@ public class ServerHandler extends ChannelInboundHandlerAdapter
 
     }
 
+    private AtomicInteger index = new AtomicInteger(0);
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception
     {
         Req req = (Req) msg;
         System.out.println("Server get msg from Client:" + req.getId() + ", " + req.getName() + ", " + req.getRequestMessage());
+
+        String path = System.getProperty("user.dir") +
+                File.separatorChar+ "receive" +
+                File.separatorChar+ "00" +
+                index.getAndIncrement()+
+                ".jpg";
+        FileOutputStream fos = new FileOutputStream(path);
+
+        byte[] attachment = GzipUtils.ungizp(req.getAttachment());
+        fos.write(attachment);
+        fos.close();
 
         Resp resp = new Resp();
         resp.setId(req.getId());
