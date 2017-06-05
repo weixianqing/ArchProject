b21c6ca3a5f7c7c60174337f97af66b30f1a3d8c
Shelvin Chow <weixianqing@gmail.com>
2017-06-05 21:00:55 +0800

    added

--- src/com/chow/arch/utils/TestSigar.java
+++ src/com/chow/arch/utils/TestSigar.java
@@ -1,7 +1,6 @@
 package com.chow.arch.utils;
 
-import org.hyperic.sigar.Sigar;
-import org.hyperic.sigar.SigarException;
+import org.hyperic.sigar.*;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -66,9 +65,157 @@ public class TestSigar
         System.out.println("用户当前工作目录：" + properties.getProperty("user.dir"));
     }
 
-    public static void memory() throws SigarException
+    private static void memory() throws SigarException
     {
         Sigar sigar = new Sigar();
+        Mem mem = sigar.getMem();
+        System.out.println("内存总量：" + mem.getTotal() / 1024l + "K ava");
+        System.out.println("当前内存使用量：" + mem.getUsed() / 2014l + "K used");
+        System.out.println("当前内存剩余量：" + mem.getFree() / 2014l + "K free");
+
+        Swap swap = sigar.getSwap();
+        System.out.println("交换区总量" + swap.getTotal() / 2014l + "K av");
+        System.out.println("当前交换区使用量：" + swap.getUsed() / 2014l + "K used");
+        System.out.println("当前交换区剩余量：" + swap.getFree() / 2014l + "K free");
+    }
+
+    private static void cpu() throws SigarException
+    {
+        Sigar sigar = new Sigar();
+        CpuInfo[] cpuInfo = sigar.getCpuInfoList();
+        CpuPerc cpuList[] = null;
+
+        System.out.println("cpu总量参数情况：" + sigar.getCpu());
+        System.out.println("cpu总百分比情况：" + sigar.getCpuPerc());
+
+
+        cpuList = sigar.getCpuPercList();
+        for (int i = 0; i < cpuInfo.length; i++)
+        {
+            CpuInfo info = cpuInfo[i];
+            System.out.println("第" + (i + 1) + "块CPU信息");
+            System.out.println("CPU的总量MHZ：" + info.getMhz());
+            System.out.println("CPU生产商：" + info.getVendor());
+            System.out.println("CPU类别：" + info.getModel());
+            System.out.println("CPU缓存数量：" + info.getCacheSize());
+            printCpuPerc(cpuList[i]);
+        }
+    }
+
+    private static void printCpuPerc(CpuPerc cpu)
+    {
+        System.out.println("CPU用户使用率：" + CpuPerc.format(cpu.getUser()));
+        System.out.println("CPU系统使用率：" + CpuPerc.format(cpu.getSys()));
+        System.out.println("CPU当前等待率：" + CpuPerc.format(cpu.getWait()));
+        System.out.println("CPU当前错误率：" + CpuPerc.format(cpu.getNice()));
+        System.out.println("CPU当前空闲率：" + CpuPerc.format(cpu.getIdle()));
+        System.out.println("CPU总使用率：" + CpuPerc.format(cpu.getCombined()));
+    }
+
+    private static void os()
+    {
+        OperatingSystem system = OperatingSystem.getInstance();
+
+        System.out.println("操作系统：" + system.getArch());
+        System.out.println("操作系统CpuEndian：" + system.getCpuEndian());
+        System.out.println("操作系统DataModel：" + system.getDataModel());
+        System.out.println("操作系统描述：" + system.getDescription());
+        System.out.println("操作系统生产商：" + system.getVendor());
+        System.out.println("操作系统生产商CodeName：" + system.getVendorCodeName());
+        System.out.println("system vendor name :" + system.getVendorName());
+        System.out.println("system vendor version :" + system.getVendorVersion());
+        System.out.println("system version : " + system.getVersion());
+    }
+
+    private static void who() throws SigarException
+    {
+        Sigar sigar = new Sigar();
+        Who[] whos = sigar.getWhoList();
+
+        if (whos != null && whos.length > 0)
+        {
+            for (int i = 0; i < whos.length; i++)
+            {
+                Who who = whos[i];
+                System.out.println("用户控制台：" + who.getDevice());
+                System.out.println("用户host：" + who.getHost());
+                System.out.println("当前系统进程表中的用户名：" + who.getUser());
+            }
+        }
+    }
+
+    private static void file() throws SigarException
+    {
+        Sigar sigar = new Sigar();
+        FileSystem[] fileSystemList = sigar.getFileSystemList();
+        for (int i = 0; i < fileSystemList.length; i++)
+        {
+            System.out.println("分区盘符名称" + i);
+            FileSystem fileSystem = fileSystemList[i];
+            System.out.println("盘符名称：" + fileSystem.getDevName());
+            System.out.println("盘符路径：" + fileSystem.getDirName());
+            System.out.println("盘符表示：" + fileSystem.getFlags());
+            System.out.println("盘符类型：" + fileSystem.getSysTypeName());
+            System.out.println("盘符类型名：" + fileSystem.getTypeName());
+            System.out.println("盘符文件系统类型：" + fileSystem.getType());
+
+            FileSystemUsage usag = sigar.getFileSystemUsage(fileSystem.getDirName());
+
+            switch (fileSystem.getType())
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    System.out.println(fileSystem.getDevName() + "总大小：" + usag.getTotal() + "Kb");
+                    System.out.println(fileSystem.getDevName() + "剩余大小：" + usag.getFree() + "Kb");
+                    System.out.println(fileSystem.getDevName() + "可用大小：" + usag.getAvail() + "Kb");
+                    System.out.println(fileSystem.getDevName() + "已经使用量：" + usag.getUsed() + "Kb");
+
+                    double usePercent = usag.getUsePercent();
+                    System.out.println(fileSystem.getDevName() + "资源利用率：" + usePercent);
+
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+            }
+
+            System.out.println(fileSystem.getDevName() + "读出" + usag.getDiskReads());
+            System.out.println(fileSystem.getDevName() + "写入" + usag.getDiskWrites());
+        }
+
+        return;
+    }
+
+    private static void net() throws SigarException
+    {
+        Sigar sigar = new Sigar();
+        String[] ifNames = sigar.getNetInterfaceList();
+
+        for (int i = 0; i < ifNames.length; i++)
+        {
+            String name = ifNames[i];
+            NetInterfaceConfig ifConfig = sigar.getNetInterfaceConfig(name);
+
+            System.out.println("网络设备名：" + name);
+            System.out.println("IP地址：" + ifConfig.getAddress());
+            System.out.println("子网掩码：" + ifConfig.getNetmask());
+
+            if ((ifConfig.getFlags() & 1l) <= 0l)
+            {
+                System.out.println("IFF_UP...skipping getNetInterfaceStat");
+                continue;
+            }
+
+            NetInterfaceStat ifStat = sigar.getNetInterfaceStat(name);
+            System.out.println(name + "接收的总包裹数：" + ifStat.getRxPackets());
+        }
     }
 }
 
